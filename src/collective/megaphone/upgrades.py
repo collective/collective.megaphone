cf72f3d37d51dab1a7f1373abc811c55a3032204
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-05 21:39:56 +0000

    fix upgrade step

--- src/collective/megaphone/upgrades.py
+++ src/collective/megaphone/upgrades.py
@@ -1,4 +1,5 @@
-PROFILE = 'collective.megaphone:default'
+PROFILE = 'profile-collective.megaphone:default'
 
 def to_1_1(context):
-    context.runImportStepFromProfile(PROFILE, 'types', run_dependencies=False, purge_old=False)
+    context.runImportStepFromProfile(PROFILE, 'typeinfo',
+                                     run_dependencies=False, purge_old=False)
