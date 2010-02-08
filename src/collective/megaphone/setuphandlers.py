b5e0465b66cb74dcf90fa2948ddaccfafd508bdd
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-08 00:05:48 +0000

    a few fixes

--- src/collective/megaphone/setuphandlers.py
+++ src/collective/megaphone/setuphandlers.py
@@ -1,10 +1,10 @@
 from Products.CMFCore.utils import getToolByName
 from collective.megaphone import HAS_PLONE40
 
-def set_add_view_expr(gscontext):
+def set_add_view_expr(context):
     # in Plone 4, the Action Letter FTI needs to have the add_view_expr set.
     if HAS_PLONE40:
-        ttool = getToolByName(gscontext, 'portal_types')
+        ttool = getToolByName(context, 'portal_types')
         ttool['Action Letter']._updateProperty(
             'add_view_expr',
             'string:${folder_url}/+/addActionLetter'
@@ -15,4 +15,5 @@ def importVarious(gscontext):
     if gscontext.readDataFile('is_megaphone_profile.txt') is None:
         return
     
-    set_add_view_expr(gscontext)
+    site = gscontext.getSite()
+    set_add_view_expr(site)
