545c4e87533d683a087a759d190e40ba3e4d77eb
davismr <davismr@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-25 16:58:24 +0000

    Add some simple setup tests

--- src/collective/megaphone/tests/testSetup.py
+++ src/collective/megaphone/tests/testSetup.py
@@ -8,9 +8,23 @@ from base import IntegrationTestCase
 class TestInstallation(IntegrationTestCase):
     """Ensure product is properly installed"""
 
+    def testCssInstalled(self):
+        assert 'wizard.css' in self.portal.portal_css.getResourceIds()
+
+    def testJsInstalled(self):
+        assert 'wizard.js' in self.portal.portal_javascripts.getResourceIds()
+
     def testSkinLayersInstalled(self):
         """"Make sure skin is installed"""
-        self.failUnless('megaphone' in self.portal.portal_skins.objectIds())
+        assert 'megaphone' in self.portal.portal_skins.objectIds()
+        assert 'wizard.css' in self.portal.portal_skins['megaphone'].objectIds()
+
+    def testMetaTypesNotToList(self):
+        assert 'LetterRecipientMailerAdapter' in self.portal.portal_properties.navtree_properties.metaTypesNotToList
+
+    def testTypesInstalled(self):
+        assert 'Action Letter' in self.portal.portal_types.objectIds()
+        assert 'LetterRecipientMailerAdapter' in self.portal.portal_types.objectIds()
 
 def test_suite():
     from unittest import TestSuite, makeSuite
