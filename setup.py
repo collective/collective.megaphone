a8a35234c403c72fa69122194d8a978c8006f897
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-06-10 18:18:45 +0000

    require TemplateFields bugfix

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(name='collective.megaphone',
           'plone.z3cform>=0.5.3',
           'plone.app.z3cform>=0.4.9',
           'Products.PloneFormGen>=1.5.0',
+          'Products.TemplateFields>=1.2.5',
           'collective.z3cform.wizard>=1.1',
           'plone.app.jquerytools>=1.0b4',
           # -*- Extra requirements: -*-
