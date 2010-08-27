fc6f10e80deab381c52693c4b39c0ef666a350b5
jbaldivieso <jbaldivieso@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-27 17:26:13 +0000

    Make sure upgrade to Plone 4 only applies to sites that have Megaphone installed.

--- src/collective/megaphone/setuphandlers.py
+++ src/collective/megaphone/setuphandlers.py
@@ -5,10 +5,11 @@ def set_add_view_expr(context):
     # in Plone 4, the Megaphone Action FTI needs to have the add_view_expr set.
     if HAS_PLONE40:
         ttool = getToolByName(context, 'portal_types')
-        ttool['Megaphone Action']._updateProperty(
-            'add_view_expr',
-            'string:${folder_url}/+/addMegaphoneAction'
-            )
+        if 'Megaphone Action' in ttool:
+            ttool['Megaphone Action']._updateProperty(
+                'add_view_expr',
+                'string:${folder_url}/+/addMegaphoneAction'
+                )
 
 def importVarious(gscontext):
     # don't run as a step for other profiles
